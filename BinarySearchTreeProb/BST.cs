b6baf36a9fed807ce57856a508fb23a13522e5e1
StudyAnkit <ankitghosh2018@gmail.com>
2022-02-26 19:08:17 +0530

    Searching a no

--- BinarySearchTreeProb/BST.cs
+++ BinarySearchTreeProb/BST.cs
@@ -82,5 +82,25 @@ namespace BinarySearchTree
             else
                 return (Size(root.left) + 1 + Size(root.right));
         }
+        public bool Search(INode<T> root, T key)
+        {
+
+            if (root == null)
+            {
+                return false;
+            }
+            if (root.data.CompareTo(key) == 0)
+            {
+                return true;
+            }
+            else if (root.data.CompareTo(key) > 0)
+            {
+                return Search(root.left, key);
+            }
+            else
+            {
+                return Search(root.right, key);
+            }
+        }
     }
 }
