2739385ed1f625ee238919c4df4d4d3b44e04672
StudyAnkit <ankitghosh2018@gmail.com>
2022-02-26 18:55:56 +0530

    Creating a tree

--- BinarySearchTreeProb/Program.cs
+++ BinarySearchTreeProb/Program.cs
@@ -1,7 +1,41 @@
 ﻿using System;
-using BinarySearchTreeProb;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
-bst.Insert(30);
-bst.Insert(70);
-bst.Display();
+namespace BinarySearchTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Program");
+            ViewBST();
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Method to Initialize Nodes.
+        /// </summary>
+        public static void ViewBST()
+        {
+            MyBinaryNode<int> mybinarynode = new MyBinaryNode<int>();
+            mybinarynode.CreateNode(56);
+            mybinarynode.CreateNode(30);
+            mybinarynode.CreateNode(70);
+            mybinarynode.CreateNode(22);
+            mybinarynode.CreateNode(40);
+            mybinarynode.CreateNode(60);
+            mybinarynode.CreateNode(95);
+            mybinarynode.CreateNode(11);
+            mybinarynode.CreateNode(65);
+            mybinarynode.CreateNode(3);
+            mybinarynode.CreateNode(16);
+            mybinarynode.CreateNode(63);
+            mybinarynode.CreateNode(67);
+            mybinarynode.Root();
+            mybinarynode.Display(mybinarynode.root);
+            Console.WriteLine("The number of nodes in BST:{0}", mybinarynode.Size(mybinarynode.root));
+        }
+    }
+}
